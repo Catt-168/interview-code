4e7a88812419362076dacfb89f44bd886d513934
Catt-168 <satpaingthu2000@gmail.com>
2024-05-26 16:38:26 +0630

    feat: added tkbo interview question

---
+++ tkbo-answer.js
@@ -0,0 +1,24 @@
+const arr = [3,7,2,4,9,10,1,6,12,30]
+arr.sort((a,v)=>a-v)
+let i = 0
+let tempArray = []
+let temp2 = []
+for(i;i<arr.length;i++){
+    let difference = arr[i+1] - arr[i]
+
+    if (difference > 2) {
+      for(let x = 1;x<difference;x++){
+           temp2.push(arr[i]+x)
+      }
+    }else {
+      tempArray.push(arr[i]+1)
+    }
+   
+}
+tempArray.pop()
+const finalArray = arr.concat(tempArray,temp2)
+finalArray.sort((a,b) => a-b)
+const uniqueArray = [...new Set(finalArray)]
+const latestArray = uniqueArray.filter(unique => !arr.includes(unique))
+console.log('Sorted array', latestArray, '\n')
+
